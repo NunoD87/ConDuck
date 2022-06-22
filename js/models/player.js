ee6807261f1190a5eff460ef25df978593611816
Nuno <nunodpmorais@gmail.com>
2022-06-22 23:44:29 +0100

    Add respawning feature

--- js/models/player.js
+++ js/models/player.js
@@ -3,6 +3,8 @@ class Player {
     this.x = CANVAS_WIDTH / 2 - PLAYER_WIDTH / 2;
     this.y = CANVAS_HEIGHT / 2 - PLAYER_HEIGHT / 2;
     this.lives = 3;
+    this.isRespawning = false;
+    this.respawningTimer = 0;
   }
 
   preload() {
@@ -11,6 +13,9 @@ class Player {
 
   draw() {
     image(this.image, this.x, this.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+    if (this.isRespawning) {
+      this.respawning();
+    }
   }
 
   move() {
@@ -18,11 +23,13 @@ class Player {
       this.y -= PLAYER_MOVEMENT_SPEED;
       if (this.y < -PLAYER_HEIGHT) {
         this.resetPosition();
+        this.isRespawning = true;
       }
     } else if (keyIsDown(ARROW_DOWN) || keyIsDown(S)) {
       this.y += PLAYER_MOVEMENT_SPEED;
       if (this.y > CANVAS_HEIGHT) {
         this.resetPosition();
+        this.isRespawning = true;
       }
     }
 
@@ -30,11 +37,13 @@ class Player {
       this.x -= PLAYER_MOVEMENT_SPEED;
       if (this.x < -PLAYER_WIDTH) {
         this.resetPosition();
+        this.isRespawning = true;
       }
     } else if (keyIsDown(ARROW_RIGHT) || keyIsDown(D)) {
       this.x += PLAYER_MOVEMENT_SPEED;
       if (this.x > CANVAS_WIDTH) {
         this.resetPosition();
+        this.isRespawning = true;
       }
     }
   }
@@ -42,5 +51,23 @@ class Player {
   resetPosition() {
     this.x = CANVAS_WIDTH / 2 - PLAYER_WIDTH / 2;
     this.y = CANVAS_HEIGHT / 2 - PLAYER_HEIGHT / 2;
+  }
+
+  respawning() {
+    if (this.respawningTimer % 5 === 0) {
+      push();
+      tint(0, 0, 0);
+      image(this.image, this.x, this.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+      pop();
+    } else {
+      noTint();
+    }
+
+    this.respawningTimer++;
+
+    if (this.respawningTimer === 50) {
+      this.isRespawning = false;
+      this.respawningTimer = 0;
+    }
   }
 }
